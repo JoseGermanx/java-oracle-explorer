059c8a2aa8c243afc2b3f6395784308c497a47cb
Jose Germán Martínez <jgermanmm@gmail.com>
2023-06-25 21:38:18 -0400

    Encapsulacion de class

--- src/duke/choice/Customer.java
+++ src/duke/choice/Customer.java
@@ -9,7 +9,23 @@ package duke.choice;
  * @author jgerm
  */
 public class Customer {
-     String name;
-     String size;
+     private String name;
+     private String size = "M";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
 
 }
